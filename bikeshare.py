effecd745192dfb343f5c191f7f1ebc464b640fe
NoufAhmed <n.a.s.m.j@hotmail.com>
2021-09-12 20:04:27 +0300

    chore:Update bikeshare file

--- bikeshare.py
+++ bikeshare.py
@@ -382,6 +382,15 @@ def main():
         city, month, day = get_filters()
 
         df = load_data(city, month, day)
+        
+        time_stats(df)
+
+        station_stats(df)
+
+        trip_duration_stats(df)             
+
+        user_stats(df,city)
+        
         # Print five rows of data
         i=0
         while True:
@@ -393,15 +402,6 @@ def main():
                   break
  
 
-        time_stats(df)
-
-        station_stats(df)
-
-        trip_duration_stats(df)             
-
-        user_stats(df,city)
-        
-        
         
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
